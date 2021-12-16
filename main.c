2b9a9d58be16208c854f78a46bc71b3ed93c55db
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2021-12-15 21:43:25 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int i = 1645497477;
+	for(;;) {
+		printf("%d ", i);
+		if(i % 2 == 0) {
+			i /= 2;
+		}
+		else if(i == 1) {
+			printf("\ndone\n");
+			break;
+		}
+		else {
+			i *= 3;
+			i++;
+		}
+	}
+	return 0;
+}
---
+++ pi.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+int r[5000 + 1];
+int c = 0;
+int g = 0;
+int b, d, i, k, z;
+
+void *multiThread(void *vargp) {
+	int *threadid = (int *)vargp;
+	static int s = 0;
+	s++;
+	g++;
+}
+
+void piCalc() {
+	for (int i = 0; i < 5000; i++) {
+		r[i] = 2000;
+	}
+	for (k = 5000; k > 0; k -= 14) {
+		d = 0;
+		i = k;
+		for (;;) {
+			d += r[i] * 10000;
+			b = 2 * i - 1;
+			r[i] = d % b;
+			d /= b;
+			i--;
+			if (i == 0) {
+				break;
+			}
+			d *= i;
+		}
+		printf("%.4d", c + d / 10000);
+		c = d % 10000;
+	}
+	printf("\n");
+}
+
+int main() {
+    	pthread_t tid;
+    	for (z = 0; z < 3; z++)
+        	pthread_create(&tid, NULL, multiThread, (void *)&tid);
+		piCalc();
+    	pthread_exit(NULL);
+    	return 0;
+}
